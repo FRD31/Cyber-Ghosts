67b23597cc23739c589ba4349fe43fdf2a95b9e6
FRD31 <fuentes.r.d.311@gmail.com>
2024-08-21 17:47:10 -0600

    si x2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-si
+// Aquí puedes agregar cualquier funcionalidad adicional que necesites
+// Ejemplo: deshabilitar clic derecho
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
